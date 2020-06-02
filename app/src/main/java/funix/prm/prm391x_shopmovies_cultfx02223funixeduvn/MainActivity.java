8bb3cb9cfa6243b4a9ed5bbac82d5241555baaee
trungcu <trungcuqn@gmail.com>
2020-06-02 20:43:15 +0700

    Create project

---
+++ app/src/main/java/funix/prm/prm391x_shopmovies_cultfx02223funixeduvn/MainActivity.java
@@ -0,0 +1,14 @@
+package funix.prm.prm391x_shopmovies_cultfx02223funixeduvn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
